64a9a67506b141d9f8cfb70ccb4baa937e3d6d41
DiegoMCastellari <agrimcastellari@gmail.com>
2020-10-15 15:30:14 -0300

    Primera Version Detalle de Producto

--- ProyectoWILDCARD/routes/index.js
+++ ProyectoWILDCARD/routes/index.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
-
+router.get('/detalleProducto', function(req, res, next) {
+  res.render('productDetail.ejs', { title: 'Express' });
+});
 module.exports = router;
