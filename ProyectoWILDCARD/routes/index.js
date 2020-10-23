7e9b51c317a9b7d3f81d2e2d7cd79c8bbe2f1e1c
Felicitas Ortiz <felicitasortiz5@gmail.com>
2020-10-23 12:29:50 -0300

    registro

--- ProyectoWILDCARD/routes/index.js
+++ ProyectoWILDCARD/routes/index.js
@@ -5,7 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
+router.get('localhost:3000', function(req, res, next) {
+  res.render('home');
+});
 router.get('/detalleProducto', function(req, res, next) {
   res.render('productDetail.ejs', { title: 'Express' });
+});
+router.get('/register', function(req, res, next) {
+  res.render('register.ejs');
 });
 module.exports = router;
