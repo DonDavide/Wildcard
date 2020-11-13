6a65fb3ebc2ca42cfbc9c50bd49565b8d0be24d1
DiegoCastellari <agrimcastellari@gmail.com>
2020-11-13 17:34:38 -0300

    Productos responsive y vista contactos

--- ProyectoWILDCARD/routes/admin.js
+++ ProyectoWILDCARD/routes/admin.js
@@ -3,9 +3,12 @@ var router = express.Router();
 const adminController = require('../controllers/adminController')
 
 router.get('/listaProductos', adminController.listaProducto);
+// post
 router.get('/nuevoProducto', adminController.nuevoProducto);
+// post
 router.get('/editarProducto', adminController.editarProducto);
+// post
+// borrar get
+// borrar post
 
 module.exports = router;
-
-
--- ProyectoWILDCARD/routes/users.js
+++ ProyectoWILDCARD/routes/users.js
@@ -2,13 +2,11 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../controllers/usersContreller')
 
-/* GET users listing. */
-/* router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-}); */
-
 router.get('/register', usersController.register);
+// post
 router.get('/login', usersController.login);
+// post
 router.get('/carrito', usersController.carrito);
+// post
 
 module.exports = router;
